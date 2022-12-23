b73a4ef20d82af2ec2ca38db76ccbbf089b52003
Ivee Jintalan <65966844+blank-183@users.noreply.github.com>
2022-12-23 21:44:37 +0800

    modify home ui

--- src/main/java/com/iveej/inventorysystem/Utils.java
+++ src/main/java/com/iveej/inventorysystem/Utils.java
@@ -11,10 +11,14 @@ import javafx.scene.control.ButtonType;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.math.BigInteger;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.Objects;
 import java.util.Optional;
 
-public class Utils extends HashPass {
+public class Utils {
 
     @FXML
     public void changeScene(ActionEvent event, String fxml, String title) {
@@ -50,6 +54,34 @@ public class Utils extends HashPass {
         alert.setContentText(contentText);
 
         return alert.showAndWait();
+    }
+
+    public byte[] getSHA(String input) throws NoSuchAlgorithmException
+    {
+        // Static getInstance method is called with hashing SHA
+        MessageDigest md = MessageDigest.getInstance("SHA-256");
+
+        // digest() method called
+        // to calculate message digest of an input
+        // and return array of byte
+        return md.digest(input.getBytes(StandardCharsets.UTF_8));
+    }
+
+    public String toHexString(byte[] hash)
+    {
+        // Convert byte array into signum representation
+        BigInteger number = new BigInteger(1, hash);
+
+        // Convert message digest into hex value
+        StringBuilder hexString = new StringBuilder(number.toString(16));
+
+        // Pad with leading zeros
+        while (hexString.length() < 64)
+        {
+            hexString.insert(0, '0');
+        }
+
+        return hexString.toString();
     }
 
     public boolean isNoBlankField(String... args) {
