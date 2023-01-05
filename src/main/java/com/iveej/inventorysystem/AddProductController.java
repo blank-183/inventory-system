98fb9a6c86274c6176cdb1f13b1d660d9a14f6a3
Ivee Jintalan <65966844+blank-183@users.noreply.github.com>
2023-01-05 23:10:28 +0800

    add field validation for add product

--- src/main/java/com/iveej/inventorysystem/AddProductController.java
+++ src/main/java/com/iveej/inventorysystem/AddProductController.java
@@ -5,6 +5,8 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 import java.net.URL;
@@ -21,9 +23,18 @@ public class AddProductController implements Initializable {
     private Button btnClose;
     @FXML
     private Button btnAdd;
-
+    @FXML
+    private TextField tfProductName;
     @FXML
     private ChoiceBox<String> cbCategory;
+    @FXML
+    private TextArea taDescription;
+    @FXML
+    private TextField tfQuantity;
+    @FXML
+    private TextField tfOrgPrice;
+    @FXML
+    private TextField tfSellPrice;
     private ArrayList<String> categories = new ArrayList<>();
 
     @Override
@@ -31,27 +42,47 @@ public class AddProductController implements Initializable {
         loadCategories();
         cbCategory.getItems().addAll(categories);
         cbCategory.setValue(categories.get(0));
+
+        tfQuantity.textProperty().addListener((observableValue, oldValue, newValue) -> {
+            if (!newValue.matches("\\d*")) {
+                tfQuantity.setText(newValue.replaceAll("\\D", ""));
+            }
+        });
+
+        tfOrgPrice.textProperty().addListener((observable, oldValue, newValue) -> {
+            if (!newValue.matches("\\d*(\\.\\d*)?")) {
+                tfOrgPrice.setText(oldValue);
+            }
+        });
+
+        tfSellPrice.textProperty().addListener((observable, oldValue, newValue) -> {
+            if (!newValue.matches("\\d*(\\.\\d*)?")) {
+                tfSellPrice.setText(oldValue);
+            }
+        });
     }
 
-    public void btnCloseAction(ActionEvent event) {
+    public void btnCloseAction() {
         Stage stage = (Stage) btnClose.getScene().getWindow();
         stage.close();
     }
 
     public void btnAddAction(ActionEvent event) {
-
+        // wait
     }
 
     public void loadCategories() {
         String query = "SELECT category_name FROM category";
 
-        try(Connection conn = ConnectDB.getConnection();
-            PreparedStatement stmt = conn.prepareStatement(query);
-            ResultSet rs = stmt.executeQuery()) {
+        try(Connection conn = ConnectDB.getConnection()) {
+            assert conn != null;
+            try(PreparedStatement stmt = conn.prepareStatement(query);
+                ResultSet rs = stmt.executeQuery()) {
 
-            while(rs.next()) {
-                String category = rs.getString("category_name");
-                categories.add(category);
+                while(rs.next()) {
+                    String category = rs.getString("category_name");
+                    categories.add(category);
+                }
             }
         } catch (SQLException e) {
             e.printStackTrace();
